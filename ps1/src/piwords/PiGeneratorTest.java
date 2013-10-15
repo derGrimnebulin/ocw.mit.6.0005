811116d5b12e60244ba2baf75021ae46d7eda713
derGrimnebulin <zftidwell@gmail.com>
2013-10-14 22:25:15 -0700

    Added test cases forpowerMod() input space 10.15.13 00:25

--- ps1/src/piwords/PiGeneratorTest.java
+++ ps1/src/piwords/PiGeneratorTest.java
@@ -9,6 +9,13 @@ public class PiGeneratorTest {
     public void basicPowerModTest() {
         // 5^7 mod 23 = 17
         assertEquals(17, PiGenerator.powerMod(5, 7, 23));
+        //m>0 or modulo is undefined.
+        assertEquals(-1,PiGenerator.powerMod(0, 0, 0));
+        //m>0 lets us cover the remaining partitions with the following test cases
+        assertEquals(-1,PiGenerator.powerMod(-1, 0, 1));
+        assertEquals(-1,PiGenerator.powerMod(-1, -1, 1));
+        assertEquals(-1, PiGenerator.powerMod(0, -1, 1));
+
     }
 
     // TODO: Write more tests (Problem 1.a, 1.c)
