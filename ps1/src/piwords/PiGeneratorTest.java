558cddd1a24a8461d03395a6a0163ef5922dc5ed
derGrimnebulin <zftidwell@gmail.com>
2013-10-17 14:27:02 -0700

    Added test suite for computePiInHex()

--- ps1/src/piwords/PiGeneratorTest.java
+++ ps1/src/piwords/PiGeneratorTest.java
@@ -9,14 +9,22 @@ public class PiGeneratorTest {
     public void basicPowerModTest() {
         // 5^7 mod 23 = 17
         assertEquals(17, PiGenerator.powerMod(5, 7, 23));
-        //m>0 or modulo is undefined.
-        assertEquals(-1,PiGenerator.powerMod(0, 0, 0));
-        //m>0 lets us cover the remaining partitions with the following test cases
-        assertEquals(-1,PiGenerator.powerMod(-1, 0, 1));
-        assertEquals(-1,PiGenerator.powerMod(-1, -1, 1));
+        assertEquals(-1, PiGenerator.powerMod(-1, 0, 1));
+        assertEquals(-1, PiGenerator.powerMod(-1, -1, 1));
         assertEquals(-1, PiGenerator.powerMod(0, -1, 1));
-
     }
+    
+    @Test
+    public void computePiInHexTest() {
+    		//test case arrays
+    		int[] a = new int[0x0]; //precision <= 0x0
+    		int[] b = new int[] {0x2,0x4}; //precision 0x2
+    		int[] c = new int[] {0x2,0x4,0x3,0xf,0x6,0xa,0x8,0x8,0x8,0x5,0xa,0x3}; //precision 0xc
+    		assertArrayEquals(a, PiGenerator.computePiInHex(0x0));
+    		assertArrayEquals(b, PiGenerator.computePiInHex(0x2));
+    		assertArrayEquals(c, PiGenerator.computePiInHex(0xc));
+     		
+  }
 
     // TODO: Write more tests (Problem 1.a, 1.c)
 }
