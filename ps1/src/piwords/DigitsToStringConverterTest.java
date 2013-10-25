ebebcf4aec52ca714c5f3e15ab347b78814c68e4
derGrimnebulin <zftidwell@gmail.com>
2013-10-24 18:01:57 -0700

    Added test suite for convertDigitsToString() 10.24.13 20:01

--- ps1/src/piwords/DigitsToStringConverterTest.java
+++ ps1/src/piwords/DigitsToStringConverterTest.java
@@ -1,4 +1,4 @@
-package piwords;
+  package piwords;
 
 import static org.junit.Assert.*;
 
@@ -14,10 +14,42 @@ public class DigitsToStringConverterTest {
         char[] alphabet = {'d', 'c', 'b', 'a'};
 
         String expectedOutput = "dcba";
-        assertEquals(expectedOutput,
-                     DigitsToStringConverter.convertDigitsToString(
-                             input, 4, alphabet));
+        assertEquals(expectedOutput, DigitsToStringConverter.convertDigitsToString(input, 4, alphabet));
+        
+        //remaining test cases for base-5 where 0 <= x_i[j] < 5
+        int[] x_1 = {0,0,0}; char[] y_1 = {'d','d','d'};
+        int[] x_2 = {0,0,1}; char[]	y_2	=	{'d','d','b'};
+        int[] x_3 = {0,1,2}; char[] y_3 = {'d','b','p'};
+        int[] x_4 = {1,2,3}; char[] y_4 = {'b','p','q'};
+        int[] x_5 = {2,3,4}; char[] y_5 = {'p','q','e'};
+        char[] xlphabet = {'b','d','p','q','e'};
+        assertEquals(y_1 ,DigitsToStringConverter.convertDigitsToString(x_1, 5, xlphabet));
+        assertEquals(y_2 ,DigitsToStringConverter.convertDigitsToString(x_2, 5, xlphabet));
+        assertEquals(y_3 ,DigitsToStringConverter.convertDigitsToString(x_3, 5, xlphabet));
+        assertEquals(y_4 ,DigitsToStringConverter.convertDigitsToString(x_4, 5, xlphabet));
+        assertEquals(y_5 ,DigitsToStringConverter.convertDigitsToString(x_5, 5, xlphabet));
+        
     }
+    
+    @Test
+  	//expected output is null if digits[i] >= base or digits[i] < 0
+    public void paramTest1() {
 
-    // TODO: Write more tests (Problem 3.a)
+    	//input 'represents' a base-3 number
+    	int[] input = {-1,0,1,2};
+    	//0 -> 'b', 1 -> 'c', 2 -> 'a'
+    	char[] alphabet = {'b', 'c', 'a'};
+    	assertEquals("", DigitsToStringConverter.convertDigitsToString(input, 3, alphabet));
+    }
+    
+    @Test
+    //expected output is null if digits[i] >= base
+    public void paramTest2() {
+    	
+    	//input 'represents' as base-2 number
+    	int[] input = {1,0,1,2};
+    	//0 -> 'c', 1 -> 'a'
+    	char[] alphabet = {'c','a'};
+    	assertEquals("", DigitsToStringConverter.convertDigitsToString(input, 2, alphabet));
+    }
 }
