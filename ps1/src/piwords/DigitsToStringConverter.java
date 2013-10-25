20336ef809572bcf6ab31acde3167343a79a9383
derGrimnebulin <zftidwell@gmail.com>
2013-10-24 20:36:10 -0700

    Added implementation of convertDigitToString(). 10.24.13 22:36

--- ps1/src/piwords/DigitsToStringConverter.java
+++ ps1/src/piwords/DigitsToStringConverter.java
@@ -16,9 +16,21 @@ public class DigitsToStringConverter {
      *                 mutated.
      * @return A String encoding the input digits with alphabet.
      */
-    public static String convertDigitsToString(int[] digits, int base,
-                                               char[] alphabet) {
-        // TODO: Implement (Problem 3.b)
-        return "";
+    public static String convertDigitsToString(int[] digits, int base, char[] alphabet) {
+    		//create char array to hold each mapped digit
+    		char[] letters = new char[digits.length];
+    		
+    		for (int i = 0; i < digits.length; i++ ) {
+    			//check validity of inputs
+    			if (digits[i] >= base || digits[i] < 0 || alphabet.length != base) 
+    				return "";
+    			
+    			//Use current digit as index and get item at index alphabet
+    			int index = digits[i];
+    			letters[i] = alphabet[index];
+    			
+    		}
+    		return new String(letters);
+    		
     }
 }
