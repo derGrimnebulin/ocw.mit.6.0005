43fa11ebf03026f1a37bd3a6eb08b23ff3af27ae
derGrimnebulin <zftidwell@gmail.com>
2013-10-29 13:47:33 -0700

    Added tests for AlphabetGenerator.Pr(). Revised test cases for AlphabetGenerator.histogram(). 10.29.13 15:47

--- ps1/src/piwords/AlphabetGeneratorTest.java
+++ ps1/src/piwords/AlphabetGeneratorTest.java
@@ -23,29 +23,51 @@ public class AlphabetGeneratorTest {
     
     @Test
     public void histogramTest() {
-      	// Expect in the training data that Pr(a) = 2/5, Pr(b) = 2/5,
-      	// Pr(c) = 1/5.
     		String[] trainingData1 = {"aa", "bbc"};
-    		char[] alphabet = {'a','b','c'};
     		Map<Character, Integer>	expectedOutput1 = new HashMap<Character, Integer>();
-    		//set values
+    		//expect the entries below to be returned
     		expectedOutput1.put('a', 2);
     		expectedOutput1.put('b', 2);
     		expectedOutput1.put('c', 1);
     		
-    		assertEquals(expectedOutput1, AlphabetGenerator.histogram(alphabet,trainingData1));
+    		assertEquals(expectedOutput1, AlphabetGenerator.histogram(trainingData1));
     		
-    		
-    		String[] trainingData2 = {"aa","bbc","zb"};
+    		String[] trainingData2 = {"aa","bbc","\nb"};
     		Map<Character, Integer>	expectedOutput2 = new HashMap<Character, Integer>();
-    		//set values
+    		//Expect the entries below to be returned
     		expectedOutput2.put('a', 2);
     		expectedOutput2.put('b', 3);
     		expectedOutput2.put('c', 1);
     		
-    		assertEquals(expectedOutput2, AlphabetGenerator.histogram(alphabet,trainingData2));
+    		assertEquals(expectedOutput2, AlphabetGenerator.histogram(trainingData2));
     		
+    }
+    
+		@Test
+    public void PrTest() {
+    		// Expect in trainingData = {"aa","bbc"} that Pr(a) = 2/5, Pr(b) = 2/5, Pr(c) = 1/5.
+    		Map<Character, Integer>	histogram1 = new HashMap<Character, Integer>();
+    		histogram1.put( 'a', 2 );
+    		histogram1.put( 'b', 2 );
+    		histogram1.put( 'c', 1 );
+    		Map<Character, Double> PDF1 = new HashMap<Character, Double>();
+    		PDF1.put( 'a', 0.4 );
+    		PDF1.put( 'b', 0.4 );
+    		PDF1.put( 'c', 0.2 );
+    		assertEquals(PDF1, AlphabetGenerator.Pr( histogram1 ));
+
     		
+    		// Expect in trainingData = {"aa","bbc","\nb"} that Pr(a) = 2/6, Pr(b) = 3/6, Pr(c) = 1/6, Pr(\n) = 0/6.
+    		Map<Character, Integer>	histogram2 = new HashMap<Character, Integer>();
+    		histogram2.put( 'a', 2 );
+    		histogram2.put( 'b', 3 );
+    		histogram2.put( 'c', 1 );
+    		Map<Character, Double> PDF2 = new HashMap<Character, Double>();
+    		PDF2.put( 'a', 0.3333333333333333 );
+    		PDF2.put( 'b', 0.5  );
+    		PDF2.put( 'c', 0.16666666666666666 );
+    		assertEquals(PDF2, AlphabetGenerator.Pr(histogram2));
+
     }
     // TODO: Write more tests (Problem 5.a)
 }
