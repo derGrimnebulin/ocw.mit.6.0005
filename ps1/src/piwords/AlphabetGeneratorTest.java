ba9292379dac71d569a7aef9bbc5e4c370c7ee17
derGrimnebulin <zftidwell@gmail.com>
2013-11-02 15:16:01 -0700

    Completed test suite 11.02.13 17:15

--- ps1/src/piwords/AlphabetGeneratorTest.java
+++ ps1/src/piwords/AlphabetGeneratorTest.java
@@ -19,8 +19,19 @@ public class AlphabetGeneratorTest {
         assertArrayEquals(expectedOutput,
                 AlphabetGenerator.generateFrequencyAlphabet(
                         10, trainingData));
+        // Expect null if base < 0.
+        assertArrayEquals(null, AlphabetGenerator.generateFrequencyAlphabet(-1, trainingData));
+        // Expect [] if base = 0;
+        
+        assertArrayEquals(new char[0], AlphabetGenerator.generateFrequencyAlphabet(0, trainingData));
+        
+        char[] expectedOutput1 = {'b'};
+        assertArrayEquals( expectedOutput1, AlphabetGenerator.generateFrequencyAlphabet(1, trainingData));
+        
+        char[] expectedOutput2 = {'a','b'};
+        assertArrayEquals( expectedOutput2, AlphabetGenerator.generateFrequencyAlphabet(2, trainingData));
     }
-    
+ 
     @Test
     public void histogramTest() {
     		String[] trainingData1 = {"aa", "bbc"};
@@ -57,17 +68,37 @@ public class AlphabetGeneratorTest {
     		assertEquals(PDF1, AlphabetGenerator.Pr( histogram1 ));
 
     		
-    		// Expect in trainingData = {"aa","bbc","\nb"} that Pr(a) = 2/6, Pr(b) = 3/6, Pr(c) = 1/6, Pr(\n) = 0/6.
+    		// Expect in trainingData = {"aa","bbc","\nb"} that Pr(a) = 2/6, 
+    		//Pr(b) = 3/6, Pr(c) = 1/6, Pr(\n) = 0/6.
     		Map<Character, Integer>	histogram2 = new HashMap<Character, Integer>();
     		histogram2.put( 'a', 2 );
     		histogram2.put( 'b', 3 );
     		histogram2.put( 'c', 1 );
     		Map<Character, Double> PDF2 = new HashMap<Character, Double>();
-    		PDF2.put( 'a', 0.3333333333333333 );
+    		PDF2.put( 'a', 0.333 );
     		PDF2.put( 'b', 0.5  );
-    		PDF2.put( 'c', 0.16666666666666666 );
+    		PDF2.put( 'c', 0.167 );
     		assertEquals(PDF2, AlphabetGenerator.Pr(histogram2));
 
     }
+		
+		@Test
+		public void CDFTest() {
+				// Expect in trainingData = {"aa","bbc","\nb"} that CDF(a) = 0.333..., 
+				// CDF(b) = 0.833..., CDF(c) = 1.0;
+				Map<Character, Double> PDF1 = new HashMap<Character, Double>();
+				PDF1.put( 'a', 0.3333 );
+				PDF1.put( 'b', 0.5 );
+				PDF1.put( 'c', 0.1667  );
+				
+				Map<Character, Double> CDF1 = new HashMap<Character, Double>();
+				CDF1.put( 'a', 0.333 );
+				CDF1.put( 'b', 0.833 );
+				CDF1.put( 'c', 1.0 );
+				
+				assertEquals(CDF1, AlphabetGenerator.CDF(PDF1));
+
+
+		}
     // TODO: Write more tests (Problem 5.a)
 }
