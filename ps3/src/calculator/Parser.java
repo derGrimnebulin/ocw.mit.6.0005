23c277c2e5ea2188a0b825b239d0ccaa39c61288
derGrimnebulin <zftidwell@gmail.com>
2014-06-01 14:27:04 -0500

    added missing class

---
+++ ps3/src/calculator/Parser.java
@@ -0,0 +1,66 @@
+package calculator;
+
+import calculator.Lexer;
+
+/*Rules of production
+ * EXP ::= NUM UNIT? OP NUM UNIT?
+ * NUM ::= digits(.digits)?
+ * OP ::= '+' | '-' | '*' | '\'
+ * UNIT ::= pts | in
+ * digits ::= [0-9]+
+ */
+
+/**
+ * Calculator parser. All values are measured in pt.
+ */
+class Parser {
+	
+	@SuppressWarnings("serial")
+	static class ParserException extends RuntimeException {
+	}
+
+	/**
+	 * Type of values.
+	 */
+	private enum ValueType {
+		POINTS, INCHES, SCALAR
+	};
+
+	/**
+	 * Internal value is always in points.
+	 */
+	public class Value {
+		final double value;
+		final ValueType type;
+
+		Value(double value, ValueType type) {
+			this.value = value;
+			this.type = type;
+		}
+
+		@Override
+		public String toString() {
+			switch (type) {
+			case INCHES:
+				return value / PT_PER_IN + " in";
+			case POINTS:
+				return value + " pt";
+			default:
+				return "" + value;
+			}
+		}
+	}
+
+	private static final double PT_PER_IN = 72;
+	private final Lexer lexer;
+
+	// TODO write method spec
+	Parser(Lexer lexer) {
+		// TODO implement for Problem 3
+	}
+
+	// TODO write method spec
+	public Value evaluate() {
+		// TODO implement for Problem 3
+	}
+}
